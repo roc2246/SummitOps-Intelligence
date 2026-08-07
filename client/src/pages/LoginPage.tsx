fbaeaea3d36fe39c0a946c45c995108d746d26c1
Riley <riley.childs@yahoo.com>
2026-08-07 13:30:23 -0400

    Add password input

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,6 +3,7 @@ import type { FormEvent } from "react";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -26,6 +27,19 @@ export default function LoginPage() {
           name="email"
           value={email}
           onChange={(event) => setEmail(event.target.value)}
+        />
+
+        <label className="login-page__label" htmlFor="password">
+          Password:
+        </label>
+
+        <input
+          className="login-page__input"
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
 
         <button className="login-page__button" type="submit">
